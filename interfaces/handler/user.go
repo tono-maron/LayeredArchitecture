9de0b67dbb7e455015d51de0dd8f08af9c7fb52f
tono-maron <tono.keigo.soccer@ezweb.ne.jp>
2019-10-29 07:32:45 +0900

    add middleware

--- interfaces/handler/user.go
+++ interfaces/handler/user.go
@@ -2,19 +2,24 @@ package handler
 
 import (
 	"LayeredArchitecture/config"
+	"LayeredArchitecture/interfaces/dddcontext"
 	"LayeredArchitecture/interfaces/response"
 	"LayeredArchitecture/usecase"
-	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func HandleUserGet(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	// Contextから認証済みのユーザIDを取得
+	ctx := request.Context()
+	//userIDが空かどうかのチェックはミドルウェアで実装してあるためここでのエラーハンドリングはない。
+	userID := dddcontext.GetUserIDFromContext(ctx)
+
 	user, err := usecase.UserUsecase{}.SelectByPrimaryKey(config.DB, userID)
 	if err != nil {
-		log.Println(err)
-		response.Error(writer, http.StatusInternalServerError, "Internal Server Error")
+		response.Error(writer, http.StatusInternalServerError, err, "Internal Server Error")
+		return
 	}
 	response.JSON(writer, http.StatusOK, user)
 }
