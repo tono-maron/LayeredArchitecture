a011e3f8c83c2b2353ecc0c7cb956fa9f5ff2ceb
tono-maron <tono.keigo.soccer@ezweb.ne.jp>
2019-10-30 03:37:06 +0900

    add post usecase Insert

--- usecase/post.go
+++ usecase/post.go
@@ -25,14 +25,18 @@ func (postUsecase PostUsecase) GetAll(DB *sql.DB) ([]domain.Post, error) {
 	return posts, nil
 }
 
-func (postUsecase PostUsecase) Insert(DB *sql.DB) ([]domain.Post, error) {
+func (postUsecase PostUsecase) Insert(DB *sql.DB, content string) error {
+	err := repository.PostRepository(persistence.PostPersistence{}).Insert(DB, content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (postUsecase PostUsecase) UpdateByPrimaryKey(DB *sql.DB, postID int) error {
 
 }
 
-func (postUsecase PostUsecase) UpdateByPrimaryKey(DB *sql.DB, postID int) ([]domain.Post, error) {
-
-}
-
-func (postUsecase PostUsecase) DeleteByPrimaryKey(DB *sql.DB, postID int) ([]domain.Post, error) {
+func (postUsecase PostUsecase) DeleteByPrimaryKey(DB *sql.DB, postID int) error {
 
 }
