80127851b9d68523a9d05a12cb7b9494f572ec82
tono-maron <tono.keigo.soccer@ezweb.ne.jp>
2019-10-29 18:57:18 +0900

    add null function

---
+++ interfaces/handler/post.go
@@ -0,0 +1,19 @@
+package handler
+
+
+
+func HandlePostGet(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+
+}
+
+func HandlePostsGet(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+
+}
+
+func HandlePostUpdate(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+
+}
+
+func HandlePostDelete(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+
+}
