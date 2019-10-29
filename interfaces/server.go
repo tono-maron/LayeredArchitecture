45e22e6eb478a8431792e47f46a373ce5822ed55
tono-maron <tono.keigo.soccer@ezweb.ne.jp>
2019-10-29 18:55:10 +0900

    add routing

--- interfaces/server.go
+++ interfaces/server.go
@@ -47,5 +47,12 @@ func Routes() *httprouter.Router {
 	router.POST("/user/signup", handler.HandleUserSignup)
 	router.POST("/user/signin", handler.HandleUserSignin)
 
+	// Post Route
+	router.GET("/post/get", middleware.Authenticate(handler.HandlePostGet))
+	router.GET("/post/index", middleware.Authenticate(handler.HandlePostsGet))
+	router.POST("/post/create", middleware.Authenticate(handler.HandlePostCreate))
+	router.PUT("/post/update", middleware.Authenticate(handler.HandlePostUpdate))
+	router.DELETE("/post/delete", middleware.Authenticate(handler.HandlePostDelete))
+
 	return router
 }
