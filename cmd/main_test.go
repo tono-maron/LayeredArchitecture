0602d086a5844632b50f7ec51a6b7ae6b21b66cd
tono-maron <tono.keigo.soccer@ezweb.ne.jp>
2019-11-28 23:15:10 +0900

    commit

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,10 +1,1 @@
 package main
-
-func TestMain() {
-	router := NewRouter()
-
-    req := httptest.NewRequest("GET", "/hello", nil)
-    rec := httptest.NewRecorder()
-
-    router.ServeHTTP(rec, req)
-}
--- interfaces/handler/main_test.go
+++ interfaces/handler/main_test.go
@@ -1,10 +1,17 @@
 package handler
 
 import (
+	"net/http/httptest"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
+	setup()
 	os.Exit(m.Run())
+}
+
+func setup() {
+	//レスポンスを生成
+	w := httptest.NewRecorder()
 }
